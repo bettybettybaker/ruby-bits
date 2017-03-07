888c008d990bc3a2b965a3cbf381b5bb412fc85c
Betty Baker <betty@heysparkbox.com>
2017-03-07 15:23:22 -0500

    Challenge: Hashes < Level 4 < Ruby Bits 1

---
+++ ruby-bits-1/lvl-4/04_hashes.rb
@@ -0,0 +1,29 @@
+=begin
+
+Challenge:
+
+Hashes
+
+Using ActiveSupport, return the difference between Mario's favorite games and Luigi's favorite games by implementing the difference_between method.
+
+=end
+
+
+#################### PROBLEM ####################
+
+
+def difference_between(player1, player2)
+
+end
+
+mario_favorite = {
+  sports: "Mario Sports Mix",
+  action: "Super Mario World"
+}
+
+luigi_favorite = {
+  sports: "Golf",
+  action: "Super Mario World"
+}
+
+puts difference_between(mario_favorite, luigi_favorite)
