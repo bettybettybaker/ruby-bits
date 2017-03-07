24ac54b02dad33f32f44b01a20c28acae9eda208
Betty Baker <betty@heysparkbox.com>
2017-03-07 14:55:18 -0500

    Challenge: Arrays II < Level 4 < Ruby Bits 1

---
+++ ruby-bits-1/lvl-4/02_arrays-2.rb
@@ -0,0 +1,19 @@
+=begin
+
+Challenge:
+
+Arrays II
+
+We have a last_games method, but we need a first_games method as well. Use Array#to to return the list of games up to Metroid. Also change the call to first_games to take in the correct index.
+
+=end
+
+
+#################### PROBLEM ####################
+
+
+def first_games(games, index)
+
+end
+games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
+puts first_games(games, 0)
