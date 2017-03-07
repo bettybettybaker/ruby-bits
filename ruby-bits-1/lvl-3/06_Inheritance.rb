d1140cf4c0f63bc3c410ef56679373db80b8ea17
Betty Baker <betty@heysparkbox.com>
2017-03-07 13:31:22 -0500

    Challenge: Inheritance < Level 3 < Ruby Bits 1

---
+++ ruby-bits-1/lvl-3/06_Inheritance.rb
@@ -0,0 +1,14 @@
+=begin
+
+Challenge:
+
+In heritance
+
+Our game class is getting a little overloaded trying to handle both console games and our old arcade console units (which are really heavy, by the way). Let's create two subclasses of Game: one for ArcadeGame and another for ConsoleGame.
+
+=end
+
+
+#################### PROBLEM ####################
+
+# Blank terminal given
