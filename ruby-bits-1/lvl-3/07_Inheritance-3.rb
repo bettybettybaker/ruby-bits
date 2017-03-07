b54f55889bb3955ac47de289d8f4f292ac8bc8e4
Betty Baker <betty@heysparkbox.com>
2017-03-07 14:11:22 -0500

    Challenge: Inheritance III < Level 3 < Ruby Bits 1

---
+++ ruby-bits-1/lvl-3/07_Inheritance-3.rb
@@ -0,0 +1,17 @@
+=begin
+
+Challenge:
+
+Inheritance III
+
+Whenever we output a game right now it'll show up using the to_s method from Object, the parent object of Game. A basic to_s implementation is completed below on Game. Override this for ConsoleGame to also show the system the game is on.
+
+=end
+
+
+#################### SOLUTION ####################
+
+
+class ConsoleGame < Game
+
+end
