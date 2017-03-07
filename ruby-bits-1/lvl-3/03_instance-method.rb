168480cdaa2de9253574097d97664d62c4765e0e
Betty Baker <betty@heysparkbox.com>
2017-03-07 11:11:03 -0500

    Challenge: Instance Method < Level 3 < Ruby Bits 1

---
+++ ruby-bits-1/lvl-3/03_instance-method.rb
@@ -0,0 +1,27 @@
+=begin
+
+Challenge:
+
+Instance Method
+
+We can initialize our Library with an array of games, but the only way to add games from outside the class is to use the games accessor method and alter the array. This is breaking encapsulation, so let's create a new method in Library called add_game which takes in a game and adds it to the games array.
+
+=end
+
+
+#################### PROBLEM ####################
+
+class Library
+  attr_accessor :games
+
+  def initialize(games)
+    self.games = games
+  end
+
+  def has_game?(search_game)
+    for game in games
+      return true if game == search_game
+    end
+    false
+  end
+end
