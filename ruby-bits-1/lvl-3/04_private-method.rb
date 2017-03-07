73893adeee41eb35d43789a0aa2e75de2d620c83
Betty Baker <betty@heysparkbox.com>
2017-03-07 11:19:20 -0500

    Challenge: Private Method < Level 3 < Ruby Bits 1

---
+++ ruby-bits-1/lvl-3/04_private-method.rb
@@ -0,0 +1,31 @@
+=begin
+
+Challenge:
+
+Private Method
+
+Things are looking good! We're able to use our Library class to store our games now. Whenever we call add_game, let's call a new private method called log which will call puts with some information about the game that was added. Your log method should take in a string to be displayed.
+
+=end
+
+
+#################### PROBLEM ####################
+
+class Library
+  attr_accessor :games
+
+  def initialize(games)
+    self.games = games
+  end
+
+  def has_game?(search_game)
+    for game in games
+      return true if game == search_game
+    end
+    false
+  end
+
+  def add_game(game)
+    games << game
+  end
+end
