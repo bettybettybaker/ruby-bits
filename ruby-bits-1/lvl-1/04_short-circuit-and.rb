cc03fad76dd3b707f39ffb78bd8a08d4c10fb027
Betty Baker <betty@heysparkbox.com>
2017-03-06 20:05:15 -0500

    Solution: Short-Circuit And < Level 1 < Ruby Bits 1

--- ruby-bits-1/lvl-1/04_short-circuit-and.rb
+++ ruby-bits-1/lvl-1/04_short-circuit-and.rb
@@ -3,7 +3,6 @@
 Challenge:
 Let's clean up our code to make it search within each game title and show all the results. If it's an exact match, we'll show something special. Clean up the code below to short circuit the if statement.
 
-
 search = "Super Mario Bros."
 games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
 matched_games = games.grep(Regexp.new(search))
