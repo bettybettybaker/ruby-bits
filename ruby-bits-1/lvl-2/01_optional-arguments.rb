92df212e9ae8607da472bb0ced496d7de84b2626
Betty Baker <betty@heysparkbox.com>
2017-03-06 20:46:31 -0500

    Solution: Optional Arguments < Level 2 < Ruby Bits 1

--- ruby-bits-1/lvl-2/01_optional-arguments.rb
+++ ruby-bits-1/lvl-2/01_optional-arguments.rb
@@ -3,6 +3,7 @@
 Challenge:
 We'll store a little more information about our games than just the name. Optional arguments are important for a flexible interface. Let's change our new_game method so it can be called without passing in year or system.
 
+
 def new_game(name, year, system)
   {
     name: name,
