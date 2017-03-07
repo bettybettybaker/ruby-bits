847d56fd6ab46a0ac7e4d9c2ca5054da679e8600
Betty Baker <betty@heysparkbox.com>
2017-03-06 20:16:14 -0500

    FIXED Solution: Conditional Assignment < Level 1 < Ruby Bits 1

--- ruby-bits-1/lvl-1/05_conditional-assignment.rb
+++ ruby-bits-1/lvl-1/05_conditional-assignment.rb
@@ -20,3 +20,9 @@ end
 
 search ||= ""
 games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
+
+matched_games = games.grep(Regexp.new(search))
+puts "Found the following games..."
+matched_games.each do |game|
+  puts "- #{game}"
+end
