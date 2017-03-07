382efc51fbb0883019e3050e39dbc86dde92b263
Betty Baker <betty@heysparkbox.com>
2017-03-07 16:00:33 -0500

    Challenge: Hashes II < Level 4 < Ruby Bits 1

---
+++ ruby-bits-1/lvl-4/05_hashes-2.rb
@@ -0,0 +1,24 @@
+=begin
+
+Challenge:
+
+Hashes II
+
+Implement the exclude_character method below to return characters except the passed in character. Use ActiveSupport to return these key/pair values. Also, change the call to exclude_character so that yoshi's games are excluded.
+
+=end
+
+
+#################### PROBLEM ####################
+
+
+def exclude_character(games, character)
+
+end
+
+games = {
+  mario: ["Super Mario World", "Super Smash Bros. Melee"],
+  luigi: ["Luigi's Mansion"],
+  yoshi: ["Yoshi's Island", "Yoshi's Story"]
+}
+puts exclude_character(games, nil)
